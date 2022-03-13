c8f8b7b50cf6c08041dc82765fc1b77bbe8250c0
Paula Zaidel <paula.zaidel@loggi.com>
2022-03-13 11:56:48 -0300

    Add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+import Navbar from "../src/components/Navbar";
+import Header from "./components/header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Main>
+        <Navbar></Navbar>
+        <Header></Header>
+      </Main>
+    </>
   );
 }
+
+const Main = styled.div`
+  width: 100vw;
+  height: auto;
+  overflow: hidden;
+`;
 
 export default App;
