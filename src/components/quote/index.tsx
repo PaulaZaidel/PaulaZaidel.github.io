09392d30317b558053a78db3e1b19247fb6dfbe2
Paula Zaidel <paula.zaidel@loggi.com>
2022-03-20 11:01:16 -0300

    QuoteComponent: Refactor

--- src/components/quote/index.tsx
+++ src/components/quote/index.tsx
@@ -7,18 +7,25 @@ const Quote = () => {
   return (
     <Container bg="black">
       <Div className="phrase-container">
-        <h2>Sendo 1% melhor todos os dias.</h2>
+        <h2>
+          "Para realizar uma grande mudança no mundo, primeiro precisamos nos
+          comprometer a uma pequena ação constante."
+        </h2>
       </Div>
     </Container>
   );
 };
 
 const Div = styled.div`
-  width: 80vw;
-  margin: 5vh;
+  margin: 1vh;
   display: flex;
   flex-direction: column;
   align-items: center;
+  text-align: center;
+
+  @media all and (min-width: 900px) {
+    width: 50vw;
+  }
 `;
 
 export default Quote;
